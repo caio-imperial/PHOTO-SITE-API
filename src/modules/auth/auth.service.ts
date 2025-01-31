1c067dbfef513e236f6106446d50acca7560a737
caio.imperial <scaioimperial@gmail.com>
2025-01-31 00:39:44 -0300

    fix: change service user for repository user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
+import { UserRepository } from '../user/domain/user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UserService,
+    @Inject('UserRepository')
+    private userRepository: UserRepository,
     private jwtService: JwtService,
   ) {}
 
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
-    const user = await this.usersService.getByEmail(email);
+    const user = await this.userRepository.getByEmail(email);
 
     const isMatch = await bcrypt.compare(pass, user.password);
     if (!isMatch) {
