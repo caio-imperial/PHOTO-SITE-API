c5e91bbb85fb592da0bfee06293b0bb3d92424fa
caio.imperial <scaioimperial@gmail.com>
2025-02-01 14:34:16 -0300

    feat: add validation on use case create user for user aready exist

--- src/modules/user/application/useCases/userCreate.useCase.ts
+++ src/modules/user/application/useCases/userCreate.useCase.ts
@@ -1,4 +1,4 @@
-import { Inject, Logger } from '@nestjs/common';
+import { BadRequestException, Inject, Logger } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserService } from '../user.service';
 import { UserRepository } from '../../domain/user.repository';
@@ -13,6 +13,14 @@ export class UserCreateUseCase {
   ) {}
 
   async execute(createUserDto: CreateUserDto): Promise<User> {
+    this.logger.log('Checking if user exist');
+    const hasUser = await this.userRepository.getByEmail(createUserDto.email);
+
+    if (hasUser) {
+      this.logger.error('User aready exist');
+      throw new BadRequestException();
+    }
+
     this.logger.log('Start to Encrypt User Password');
     const hashedPassword = await this.userService.hashedPassword(
       createUserDto.password,
