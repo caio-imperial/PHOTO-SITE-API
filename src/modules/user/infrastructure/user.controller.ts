dd6587626121f8ea3eba8b3eedfa87f8e409b6fe
caio.imperial <scaioimperial@gmail.com>
2025-02-01 14:34:41 -0300

    refactor: change import in user controller for jest

--- src/modules/user/infrastructure/user.controller.ts
+++ src/modules/user/infrastructure/user.controller.ts
@@ -9,9 +9,9 @@ import {
   Request,
   UnauthorizedException,
 } from '@nestjs/common';
-import { UserPublicPresentations } from './presentations/userPublic.presentations';
+import { UserPublicPresentations } from './presentations/user-public.presentations';
 import { UserCreateUseCase } from '../application/useCases/userCreate.useCase';
-import { Public } from 'src/modules/auth/application/decorators/public.decorator';
+import { Public } from '../../auth/application/decorators/public.decorator';
 import { CreateUserDto } from '../application/dto/create-user.dto';
 import { UserGetUseCase } from '../application/useCases/userGet.useCase';
 import { UserNotFoundException } from '../domain/exceptions/userNotFound.exception';
